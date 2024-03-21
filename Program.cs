0a88c5390d607b61137c704c44d34d1ef5014a84
AfruzCoder <husanovafruz1@gmail.com>
2024-03-21 11:56:33 +0500

    IeltsScorecalculation

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double speakingMark, readingMark, writingMark, listeningMark;
+
+        // Foydalanuvchidan imtihon ballarini kiritish
+        Console.Write("Inter your Listening score: ");
+        speakingMark = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Inter your reading score: ");
+        readingMark = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Inter your writing score: ");
+        writingMark = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Inter your Listening score: ");
+        listeningMark = Convert.ToDouble(Console.ReadLine());
+
+        // All score calculation
+        double totalMarks = speakingMark + readingMark + writingMark + listeningMark;
+
+        // Ortacha ballni hisoblash
+        double averageMark = totalMarks / 4;
+
+
+        Console.WriteLine($"average score: {averageMark}");
+        Console.Write("Is countiniu? (yes/no): ");
+        string scaultiniu = Console.ReadLine();
+
+        if (scaultiniu.ToLower() == "yes")
+        {
+            Main();
+        }
+        else if (scaultiniu.ToLower() == "no")
+        {
+            Console.WriteLine("Dastur to'xtatildi.");
+        }
+        else
+        {
+            Console.WriteLine("ERROR.");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# IletsMark
+## Bu darsur siz ielts teacherlari tahlilidan oldinroq uzingiz tahlil qilishingizga yordam beradi 
+
+## IELTS Natijalari
+
+Bu repozitoriyaning maqsadi: IELTS  imtihonining natijalarini o'z ichiga olish.
+
+## Imtihon Turi
+
+IELTS imtihoni 4 ta bo'limdan iborat:
+
+1. Listening: Tinglash
+2. Reading: O'qish
+3. Writing: Yozish
+4. Speaking: Nutq
+
+## Natijalar
+### Masalan:
+- Listening: 7.5
+- Reading: 8.0
+- Writing: 7.0
+- Speaking: 7.5
+
+_Bunda ko'ra biz shu imtihonlardan yig'ilgan ballarni ielts balinigiz nechchi chiqishini oldindan bilamiz_
+
+
+_Bunda ko'ra biz shu imtihonlardan yig'ilgan ballarni ielts balinigiz nechchi chiqishini oldindan bilamiz_
+
+## Image
+![Menu](/Images/menu.png)
+
+![code running](/Images/code-run.gif)
+## Muallif
+
+Muallif: [***Husanov Afruzbek***]
